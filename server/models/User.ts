2cb0af63e1c5c04ae882c51e0852735459562397
UfukCemDELICE <ufukcemdelice02@gmail.com>
2023-05-22 23:55:09 +0300

    user model added

---
+++ server/models/User.ts
@@ -0,0 +1,14 @@
+import mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('user', UserSchema);
